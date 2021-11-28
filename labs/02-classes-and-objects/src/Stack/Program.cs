fa148858ccf2589c36c73e46f82fc120b8bc3216
Hung Nguyen <hungdhnt@gmail.com>
2021-11-28 19:40:44 +0700

    fix naming

--- labs/02-classes-and-objects/src/Stack/Program.cs
+++ labs/02-classes-and-objects/src/Stack/Program.cs
@@ -8,7 +8,7 @@ class Program
         int n, n0;
 
         // Khởi tạo ngăn xếp
-        Stack s = new Stack();
+        Stack stack1 = new Stack();
 
         Console.Write("Nhap so tu nhien n: ");
         n = int.Parse(Console.ReadLine());
@@ -20,14 +20,14 @@ class Program
             // Chừng nào n còn chia hết cho i -> đẩy i vào ngăn xếp, giảm n = n/i
             while(n % i == 0)
             {
-                s.Push(i);
+                stack1.Push(i);
                 n /= i;
             }
         }
 
         // In ra các phần tử trong ngăn xếp:
         Console.WriteLine("So {0} phan tich ra thua so nguyen to:", n0);
-        s.Print();
+        stack1.Print();
 
         // Đổi số n sang hệ 16
         n = n0; // gán lại giá trị ban đầu của n
@@ -38,12 +38,14 @@ class Program
         {
             // Lấy ra số dư của phép chia n/16
             int soDu = n % 16;
+            // Đẩy số dư vào stack
             stack2.Push(soDu);
+            // Giảm n
             n = n / 16;
         }
 
         // In ra ngăn xếp (chứa các số dư theo thứ tự ngược lại)
-        Console.WriteLine("\nSo {0} doi sang he 16 la", n0);
+        Console.WriteLine("\nSo {0} doi sang he 16 la:", n0);
         stack2.PrintHex();
     }
 }
