207532861fd0238acc4dc556ee628fb2170707cc
Hung Nguyen <hungdhnt@gmail.com>
2024-03-12 17:08:44 +0700

    Add content

--- site/labs/04-polymorphism/code/Stack2/Program.cs
+++ site/labs/04-polymorphism/code/Stack2/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     // Hàm PhanTichThua số nhận vào số tự nhiên n 
     // và trả về ngăn xếp chứa các thừa số nguyên tố của n
-    public static PrimeStack PhanTichThuaSo(int n)
+    public static Stack PhanTichThuaSo(int n)
     {
-        PrimeStack stack = new PrimeStack();
+        Stack stack = new PrimeStack();
         for(int i=2; i <=n; i++)
         {
             while(n % i ==0)
@@ -18,12 +16,11 @@ class Program
         return stack;
     }
 
-
     // Hàm Dec2Hex số nhận vào số tự nhiên n 
     // và trả về ngăn xếp chứa các dư số của n chia cho 16
-    public static HexaStack Dec2Hex(int n)
+    public static Stack Dec2Hex(int n)
     {
-        HexaStack stack = new HexaStack();
+        Stack stack = new HexaStack();
         while(n > 0)
         {
             int soDu = n % 16;
@@ -33,6 +30,7 @@ class Program
         return stack;
     }
 
+    // Chương trình chính
     static void Main(string[] args)
     {
         int n;
@@ -43,14 +41,16 @@ class Program
             n = int.Parse(Console.ReadLine());
         }while(n < 2);
         
-        // Phan tich n ra thua so nguyen to
-        PrimeStack stack1 = PhanTichThuaSo(n);
+        // Phân tích n thành thừa số nguyên tô
+        Stack stack1 = PhanTichThuaSo(n);
         Console.Write("{0} = ", n);
+        // In ra kết quả
         stack1.Print();
 
-        // Doi n sang he 16
-        HexaStack stack2 = Dec2Hex(n);
+        // Đổi n sang hệ 16
+        Stack stack2 = Dec2Hex(n);
         Console.Write("So {0} bieu dien trong he 16 la ", n);
+        // In kết quả
         stack2.Print();
     }
 }
